82c87f9e2e8be40c8a0984bf286e8a073b91479d
mcuma <m.cuma@utah.edu>
2018-09-28 10:29:28 -0600

    script updates

--- Comsol_Ember/comsol_ember.sh
+++ Comsol_Ember/comsol_ember.sh
@@ -15,5 +15,9 @@ module load comsol
 
 comsol batch -inputfile /uufs/chpc.utah.edu/sys/pkg/comsol/4.4/models/COMSOL_Multiphysics/Equation-Based_Models/black_scholes_put.mph -outputfile my_test.mph
 
+# optionally if the intermediate files are large, we need to write them to a different directory to avoid running out of home directory quota
+# based on https://scicomp.ethz.ch/wiki/Comsol_files_in_the_home_directory
+# export TMPDIR /scratch/general/lustre/$USER/$SLURM_JOBID
+# comsol batch -tmpdir $TMPDIR -configuration $TMPDIR/configuration/comsol_@process.id -data $TMPDIR/data/comsol_@process.id -prefsdir $TMPDIR/preferences/comsol_@process.id -inputfile infile.mph -outputfile outfile.mph
 
 
--- Lumerical_Ember/lumerical_ember.sh
+++ Lumerical_Ember/lumerical_ember.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=10:00
 
 cd $SLURM_SUBMIT_DIR
-module load intel/2016.3.210 impi/5.1.3 lumerical/8.16.1022
+module load intel impi lumerical
 
 cp /uufs/chpc.utah.edu/sys/installdir/lumerical/fdtd/8.16.1022/examples/nanowire.fsp .
 
